da2841281e6d962732b80e0ef43ae9816cc09cd5
Henri Egger <henri@moosbauer.com>
2024-11-24 18:59:49 +0100

    formatting

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,16 @@
 // Import our custom CSS
-import '../scss/styles.scss'
+import "../scss/styles.scss";
 
 // Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
+import * as bootstrap from "bootstrap";
 
-import { renderPlot } from './render';
-import { handleSubmit, updateForm } from './form';
+import { renderPlot } from "./render";
+import { handleSubmit, updateForm } from "./form";
 
 const componentsForm = document.getElementById("components-form");
 componentsForm.addEventListener("submit", handleSubmit);
 
 window.onload = () => {
-  updateForm()
-  renderPlot()
+  updateForm();
+  renderPlot();
 };
